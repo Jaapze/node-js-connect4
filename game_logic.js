064e90e570a9960d54aa95d3365ebf8c060b7029
atticweb <zolderweb@gmail.com>
2015-04-14 12:17:41 +0200

    added game_logic file to clear up server.js

---
+++ game_logic.js
@@ -0,0 +1,88 @@
+module.exports = {
+	check_for_win : function(board){
+		var found = 0,
+		winner_coins = [],
+		winner = false,
+		data = {},
+		person = 0;
+		/*horizontal*/
+		for(var row = 0; row < board.length; row++){
+			if(winner) break;
+			found = 0;
+			person = 0;
+			for(var col = 0; col < board[row].length; col++){
+				var selected = board[row][col];
+				if(selected !== 0) found = (person != selected) ? 1 : found + 1;
+				person = selected;
+				if(found >= 4){
+					winner = person;
+					for(var k = 0; k < 4; k++){
+						winner_coins[k] = row+''+(col-k);
+					}
+				}
+				if((col > 2 && found == 0) || found >= 4) break;
+			}
+		}
+		/*vertical*/
+		if(!winner){
+			for(col = 0; col < board[0].length; col++){
+				if(winner) break;
+				found = 0;
+				person = 0;
+				for(row = 0; row < board.length; row++){
+					var selected = board[row][col];
+					if(selected !== 0) found = (person != selected) ? 1 : found + 1;
+					person = selected;
+					if(found >= 4){
+						winner = person;
+						for(var k = 0; k < 4; k++){
+							winner_coins[k] = (row-k)+''+col;
+						}
+					}
+					if((row > 1 && found == 0) || found >= 4) break;
+				}
+			}
+		}
+		/*diagonal left-up->right*/
+		if(!winner){
+			for(col = 0; col < board[0].length-3; col++){
+				if(winner) break;
+				for(row = 0; row < board.length-3; row++){
+					var first_val = board[row][col];
+					if(first_val == 0) continue;
+					if(	first_val === board[row+1][col+1] &&
+						first_val === board[row+2][col+2] &&
+						first_val === board[row+3][col+3] ){
+						winner = first_val;
+						winner_coins = [row+''+col,(row+1)+''+(col+1),(row+2)+''+(col+2),(row+3)+''+(col+3)];
+						break;
+					}
+				}
+			}
+		}
+		/*diagonal right-up->left*/
+		if(!winner){
+			for(col = board[0].length-1; col > 2; col--){
+				if(winner) break;
+				for(row = 0; row < board.length-3; row++){
+					var first_val = board[row][col];
+					if(first_val == 0) continue;
+					if(	first_val === board[row+1][col-1] &&
+						first_val === board[row+2][col-2] &&
+						first_val === board[row+3][col-3] ){
+						winner = first_val;
+						winner_coins = [row+''+col,(row+1)+''+(col-1),(row+2)+''+(col-2),(row+3)+''+(col-3)];
+						break;
+					}
+				}
+			}
+		}
+		
+		if(winner) {
+			data.winner = winner;
+			data.winner_coins = winner_coins;
+			return data;
+		}
+		return false;
+	}
+}
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //server needs
 var express = require('express'),
+	game_logic = require('./game_logic'),
 	app = express(),
 	server = require('http').createServer(app),
 	port = Number(process.env.PORT || 3000),
@@ -36,92 +37,6 @@ function generateHash(length) {
 	return output;
 };
 
-function check_for_win(board){
-	var found = 0,
-		winner_coins = [],
-		winner = false,
-		data = {},
-		person = 0;
-	/*horizontal*/
-	for(var row = 0; row < board.length; row++){
-		if(winner) break;
-		found = 0;
-		person = 0;
-		for(var col = 0; col < board[row].length; col++){
-			var selected = board[row][col];
-			if(selected !== 0) found = (person != selected) ? 1 : found + 1;
-			person = selected;
-			if(found >= 4){
-				winner = person;
-				for(var k = 0; k < 4; k++){
-					winner_coins[k] = row+''+(col-k);
-				}
-			}
-			if((col > 2 && found == 0) || found >= 4) break;
-		}
-	}
-	/*vertical*/
-	if(!winner){
-		for(col = 0; col < board[0].length; col++){
-			if(winner) break;
-			found = 0;
-			person = 0;
-			for(row = 0; row < board.length; row++){
-				var selected = board[row][col];
-				if(selected !== 0) found = (person != selected) ? 1 : found + 1;
-				person = selected;
-				if(found >= 4){
-					winner = person;
-					for(var k = 0; k < 4; k++){
-						winner_coins[k] = (row-k)+''+col;
-					}
-				}
-				if((row > 1 && found == 0) || found >= 4) break;
-			}
-		}
-	}
-	/*diagonal left-up->right*/
-	if(!winner){
-		for(col = 0; col < board[0].length-3; col++){
-			if(winner) break;
-			for(row = 0; row < board.length-3; row++){
-				var first_val = board[row][col];
-				if(first_val == 0) continue;
-				if(	first_val === board[row+1][col+1] &&
-					first_val === board[row+2][col+2] &&
-					first_val === board[row+3][col+3] ){
-					winner = first_val;
-					winner_coins = [row+''+col,(row+1)+''+(col+1),(row+2)+''+(col+2),(row+3)+''+(col+3)];
-					break;
-				}
-			}
-		}
-	}
-	/*diagonal right-up->left*/
-	if(!winner){
-		for(col = board[0].length-1; col > 2; col--){
-			if(winner) break;
-			for(row = 0; row < board.length-3; row++){
-				var first_val = board[row][col];
-				if(first_val == 0) continue;
-				if(	first_val === board[row+1][col-1] &&
-					first_val === board[row+2][col-2] &&
-					first_val === board[row+3][col-3] ){
-					winner = first_val;
-					winner_coins = [row+''+col,(row+1)+''+(col-1),(row+2)+''+(col-2),(row+3)+''+(col-3)];
-					break;
-				}
-			}
-		}
-	}
-	
-	if(winner) {
-		data.winner = winner;
-		data.winner_coins = winner_coins;
-		return data;
-	}
-	return false;
-}
 
 function make_move(board, col, pid){
 	var move_made = false;
@@ -180,7 +95,7 @@ io.sockets.on('connection', function(socket){
 					games[socket.room].moves = parseInt(games[socket.room].moves)+1;
 					socket.broadcast.to(socket.room).emit('move_made', {pid: socket.pid, col: data.col});
 					games[socket.room].turn = socket.opponent.pid;
-					var winner = check_for_win(games[socket.room].board);
+					var winner = game_logic.check_for_win(games[socket.room].board);
 					if(winner){
 						io.to(socket.room).emit('winner', {winner: winner});
 					}
